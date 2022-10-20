47433c4a3d656f5f1332d771a2e8f5a0f0958faa
Oiga Serik <Kharitonova.87@mail.ru>
2022-10-20 13:35:04 +0300

    3

---
+++ Num3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+
+ввод элеменетов через консоль (через запятую. использовать метод string.Split())
+*/
+
+
+Console.WriteLine("Введите числа: ");
+string[] Array = Console.ReadLine().Split(", ");
+int[] NumArray = new int[Array.Length];
+for (int i = 0; i < Array.Length; i++)
+{
+    NumArray[i] = Int32.Parse(Array[i]);
+}
+Console.WriteLine("[{0}]", string.Join(", ", NumArray));
